4221f2ba0238b86ce4b3d22b6ed73e5c7ea4c66b
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-10-20 23:06:25 +0300

    fix: fix carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,7 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
   if (typeof (sampleActivity) != 'string') {
     return false;
   }
@@ -17,5 +17,4 @@ module.exports = function dateSample(/* sampleActivity */) {
   const k = 0.693 / HALF_LIFE_PERIOD;
   let ln = Math.log10(MODERN_ACTIVITY / parseString);
   return Math.floor(ln / k);
-
 };
