f5bb5015867ce109e4429dcbf5f84c06feb8b532
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-11-01 19:30:47 +0300

    fix: fix solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,10 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
-  if(Array.isArray(members)) {
-    return members.filter((item) => typeof item == 'string')
-    .map (item => item.trim());
-    .map(item => item[0].toUpperCase()).sort().join('');
-  }
-  return false;
-};
+    if(Array.isArray(members)){
+      return members.filter((item) => typeof item == "string")
+     .map(item => item.trim())
+     .map(item => item[0].toUpperCase()).sort().join('')
+    }
+   return false
+  };
