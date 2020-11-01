32007fbefd7805afd072d1077b83a21bf8c4b7fe
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-11-01 23:08:02 +0300

    fix: fix solution

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -2,7 +2,7 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
   const result = {};
-  result.turns = Math.pow(2, disksNumber) - 1;
+  result.turns = ((2 ** disksNumber) - 1);
   result.seconds = Math.floor((result.turns * 3600) / turnsSpeed);
 
   return result;
