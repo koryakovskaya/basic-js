48e42ea960c74dc0575150bb28a33ab6efd56929
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-11-01 23:17:19 +0300

    fix: fix solution

--- src/what-season.js
+++ src/what-season.js
@@ -1,29 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
-  if (date == null) return 'Unable to determine the time of year!';
-  if(isNaN(date)) return 'THROWN';
+  if (date == null) return "Unable to determine the time of year!";
+  if (isNaN(date)) return "THROWN";
 
   let season;
-
   switch (date.getMonth()) {
     case 11:
     case 0:
-    case 1: 
-      season = 'winter';
+    case 1:
+      season = "winter";
       break;
     case 2:
     case 3:
     case 4: 
-      season = 'spring';
+      season = "spring";
       break;
     case 5:
     case 6:
     case 7:
-      season = 'summer';
+      season = "summer";
       break;
     default:
-      season = 'autumn';
+      season = "autumn";
       break;
   }
 
