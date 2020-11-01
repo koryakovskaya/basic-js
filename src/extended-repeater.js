7ab66cca8d80a21225f84239731d5410515870f6
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-11-01 22:19:34 +0300

    fix: fix solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,27 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
+  
   let {
-    repTimes = 0, 
+    repTimes = 0,
     separator = '+',
     addition = '',
     additionRepTimes = 0,
     additionSep = '|'
   } = options;
-
+  
   let sep = [];
   let solution = [];
 
   str = String(str);
   addition = String(addition);
 
-  if(!repTimes) return str + addition;
-  for(let i = 0; i < additionRepTimes; i++) {
+  if (!repTimes) return str + addition;
+
+  for (let i = 0; i < additionRepTimes; i++) {
     sep.push(addition);
   }
 
   sep = additionRepTimes ? sep.join(additionSep) : addition;
-  for(let i = 0; i < repTimes; i++) {
+
+  for (let i = 0; i < repTimes; i++) {
     solution.push(str + sep);
   }
 
