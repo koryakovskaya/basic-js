a65b2582ccfdd7a28df572242a7179dc5c765cff
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-11-01 19:21:51 +0300

    feat: add solution to the dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(Array.isArray(members)) {
+    return members.filter((item) => typeof item == 'string')
+    .map (item => item.trim());
+    .map(item => item[0].toUpperCase()).sort().join('');
+  }
+  return false;
 };
