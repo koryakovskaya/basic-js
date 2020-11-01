02e5160a964ad3f02ad1d50a627f52dec8dbf257
koryakovskayafeat: add solutions <koryakovskaya.tatiana@gmail.com>
2020-11-01 22:52:18 +0300

    feat: add solution tranfrom array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,50 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
+module.exports = function transform(arr) {
   throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+
+  let solution = arr.slice();
+  let res = [];
+
+  if ((Array.isArray(arr)) {
+    if (arr.length === 0) {
+      return [];
+    } else {
+      for (let i = 0; i < solution.length; i++) {
+        if (solution[i] === '--discard-next') {
+          if (solution[i + 1] || solution[i + 1] === false || solution[i + 1] === 0 || isNaN(solution[i + 1]) ) {
+            delete solution[i + 1];
+            solution.splice(i, 1);
+          } else {
+            solution.splice(i, 1);
+          }
+        } else if (solution[i] === '--discard-prev') {
+          if (solution[i - 1] !== undefined) {
+            delete solution[i - 1];
+            solution.splice(i, 1);
+          } else {
+            solution.splice(i, 1);
+          }
+
+        } else if (solution[i] === '--double-prev') {
+            if (solution[i - 1] !== undefined) {
+              solution[i] = solution[i - 1];
+            } else {
+              solution.splice(i, 1);
+            }
+        }
+    }
+
+    for(let k = 0; k < solution.length; k++) {
+      if (solution[k] !== undefined) {
+          res.push(solution[k]);
+      }
+  }
+
+  return res;
+}
+} else {
+  
+  throw 'Error';
+}
 };
